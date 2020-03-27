956c72a392308095f2d146845e4f760eb997d547
zionrub88 <z.rub.88@gmail.com>
2020-03-27 13:46:04 +0300

    create cloud formation stack

--- rainbow/runners/airflow/tasks/create_cloudformation_stack.py
+++ rainbow/runners/airflow/tasks/create_cloudformation_stack.py
@@ -15,8 +15,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
 
 from rainbow.runners.airflow.model import task
+from rainbow.runners.airflow.operators.cloudformation import CloudFormationHook, CloudFormationCreateStackOperator, \
+    CloudFormationCreateStackSensor
+
+from flatdict import FlatDict
 
 
 class CreateCloudFormationStackTask(task.Task):
@@ -26,6 +32,72 @@ class CreateCloudFormationStackTask(task.Task):
 
     def __init__(self, dag, pipeline_name, parent, config, trigger_rule):
         super().__init__(dag, pipeline_name, parent, config, trigger_rule)
+        self.stack_name = config['name']
+        self.template_url = config['template_url']
+        self.time_out_in_minutes = config.get('time_out_in_minutes', 25)
+        self.parameters = self.__get_parameters()
 
     def apply_task_to_dag(self):
-        pass
+
+        check_cloudformation_stack_exists_task = BranchPythonOperator(
+            task_id='is_cloudformation_stack_running',
+            python_callable=self.__cloudformation_stack_running_branch,
+            provide_context=True,
+            dag=self.dag
+        )
+
+        create_cloudformation_stack_task = CloudFormationCreateStackOperator(
+            task_id='create_cloudformation_stack',
+            params={
+                'StackName': self.stack_name,
+                'TimeoutInMinutes': self.time_out_in_minutes,
+                'Capabilities': ['CAPABILITY_NAMED_IAM'],
+                'TemplateURL': self.template_url,
+                'Parameters': self.parameters,
+                'OnFailure': 'DO_NOTHING'
+            },
+            dag=self.dag
+        )
+
+        create_stack_sensor_task = CloudFormationCreateStackSensor(
+            task_id='cloudformation_watch_cluster_create',
+            stack_name=self.stack_name,
+            dag=self.dag
+        )
+
+        stack_creation_end_task = DummyOperator(
+            task_id='stack_creation_end',
+            dag=self.dag,
+            trigger_rule=self.trigger_rule
+        )
+
+        if self.parent:
+            self.parent.set_downstream(check_cloudformation_stack_exists_task)
+
+        check_cloudformation_stack_exists_task.set_downstream(stack_creation_end_task)
+        check_cloudformation_stack_exists_task.set_downstream(create_cloudformation_stack_task)
+        create_cloudformation_stack_task.set_downstream(create_stack_sensor_task)
+
+        create_stack_sensor_task.set_downstream(stack_creation_end_task)
+
+    def __cloudformation_stack_running_branch(self, **kwargs):
+        cloudformation = CloudFormationHook().get_conn()
+        try:
+            stack_status = cloudformation.describe_stacks(StackName=self.stack_name)['Stacks'][0]['StackStatus']
+            if stack_status in ['CREATE_COMPLETE', 'DELETE_FAILED']:
+                print(f'Stack {self.stack_name} is running')
+                return 'stack_creation_end'
+            else:
+                print(f'Stack {self.stack_name} is not running')
+        except Exception as e:
+            if 'does not exist' in str(e):
+                print(f'Stack {self.stack_name} does not exist')
+                return 'create_cloudformation_stack'
+            else:
+                raise e
+
+        return 'create_cloudformation_stack'
+
+    def __get_parameters(self):
+        return [{'ParameterKey': x, 'ParameterValue': y} for (x, y) in
+                FlatDict(self.config['parameters']).items()]
---
+++ tests/runners/airflow/tasks/test_create_cloud_formation_stack.py
@@ -0,0 +1,104 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+from unittest import TestCase
+
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
+
+from rainbow.runners.airflow.operators.cloudformation import CloudFormationCreateStackOperator, \
+    CloudFormationCreateStackSensor
+from rainbow.runners.airflow.operators.kubernetes_pod_operator_with_input_output import \
+    KubernetesPodOperatorWithInputAndOutput
+from rainbow.runners.airflow.tasks import python, create_cloudformation_stack
+from tests.util import dag_test_utils
+
+
+class TestCreateCloudFormationStack(TestCase):
+
+    def test_apply_task_to_dag(self):
+        # TODO: elaborate tests
+        self.dag = dag_test_utils.create_dag()
+
+        task_id = 'my_task'
+
+        config = self.__create_conf(task_id)
+
+        task0 = create_cloudformation_stack.CreateCloudFormationStackTask(self.dag, 'my_pipeline', None, config,
+                                                                          'all_done')
+        task0.apply_task_to_dag()
+
+        # Test metadata
+        self.assertEqual(task0.trigger_rule, 'all_done')
+        self.assertEqual(task0.stack_name, task_id)
+        self.assertEqual(task0.time_out_in_minutes, 35)
+        self.assertEqual(task0.parameters, [{'ParameterKey': 'core_servers', 'ParameterValue': 2},
+                                            {'ParameterKey': 'emr_version', 'ParameterValue': 'emr-5.28.0'}])
+
+        # Test dag tests
+        self.assertEqual(len(self.dag.tasks), 4)
+
+        self.__test_is_cloudformation_stack_running(self.dag.tasks[0])
+
+        self.__test_create_cloudformation_stack(self.dag.tasks[1])
+
+        self.__test_cloudformation_watch_cluster_create(self.dag.tasks[2])
+
+        self.__test_stack_creation_end(self.dag.tasks[3])
+
+    def __test_is_cloudformation_stack_running(self, task):
+        self.assertIsInstance(task, BranchPythonOperator)
+        self.assertEqual(task.task_id, 'is_cloudformation_stack_running')
+
+        downstream_lst = task.downstream_list
+        # The order of the downstream tasks here does not matter. sorting is just to keep it deterministic for tests
+        downstream_lst.sort()
+        self.assertEqual(len(downstream_lst), 2)
+
+        self.__test_create_cloudformation_stack(downstream_lst[0])
+        self.__test_stack_creation_end(downstream_lst[1])
+
+    def __test_create_cloudformation_stack(self, task):
+        self.assertIsInstance(task, CloudFormationCreateStackOperator)
+        self.assertEqual(task.task_id, 'create_cloudformation_stack')
+
+    def __test_cloudformation_watch_cluster_create(self, task):
+        self.assertIsInstance(task, CloudFormationCreateStackSensor)
+        self.assertEqual(task.task_id, 'cloudformation_watch_cluster_create')
+
+    def __test_stack_creation_end(self, task):
+        self.assertIsInstance(task, DummyOperator)
+        self.assertEqual(task.task_id, 'stack_creation_end')
+
+    @staticmethod
+    def __create_conf(task_id):
+        return {
+            'name': task_id,
+            'template_url': 'foo bar',
+            'persistent': False,
+            'time_out_in_minutes': 35,
+            'parameters': {
+                'core_servers': 2,
+                'emr_version': 'emr-5.28.0'
+            }
+        }
+
+
+if __name__ == '__main__':
+    unittest.main()
