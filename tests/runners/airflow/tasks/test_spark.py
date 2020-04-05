82782e5e391601c153473e1298a861abc2a08aa1
zionrub88 <z.rub.88@gmail.com>
2020-04-05 18:33:54 +0300

    fix test spark

--- tests/runners/airflow/tasks/test_spark.py
+++ tests/runners/airflow/tasks/test_spark.py
@@ -28,9 +28,10 @@ class TestSparkTask(TestCase):
         self.assertEqual(dag_task0.aws_conn_id, 'aws_ni')
 
         self.assertEqual(dag_task0.cluster_states, ['RUNNING', 'WAITING'])
+
         dag_task1 = dag.tasks[1]
         self.assertIsInstance(dag_task1, EmrStepSensor)
-        print(task0.spark_submit)
+
         expected_spark_submit = ['spark-submit', '--name', 'hello_spark', '--deploy_mode', 'standalone', '--class',
                                  '.class', '--conf', 'spark.sql.parquet.writeLegacyFormat=true', '--conf',
                                  'spark.driver.cores=3', 'source_jar',
